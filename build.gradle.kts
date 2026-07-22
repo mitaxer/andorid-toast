0c72f02648042fdd6ebba12f970ed7a7ce24dbeb
Mmx <373765724@qq.com>
2026-07-22 15:37:04 +0800

    fix: add allprojects repositories in root build

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("com.android.application") version "8.3.0" apply false
     id("com.android.library") version "8.3.0" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
